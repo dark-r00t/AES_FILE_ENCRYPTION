8cfc25921224fdab3b7d8e1172e2b65ba6c1ee58
Jan D <79925808+dark-r00t@users.noreply.github.com>
2022-07-19 23:13:17 -0400

    Update aes.c

--- src/aes.c
+++ src/aes.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <string.h>
 #include "aes.h"
+#include "general.h"
 
 static unsigned char rcon[256] = {
     0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36, 0x6c, 0xd8, 0xab, 0x4d, 0x9a,
@@ -24,6 +25,25 @@ static unsigned char rcon[256] = {
 };
 
 uint8_t s_box[256];
+
+unsigned char inv_s[256] = { // TODO
+    0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB,
+    0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB,
+    0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E,
+    0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25,
+    0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92,
+    0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84,
+    0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06,
+    0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B,
+    0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73,
+    0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E,
+    0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B,
+    0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4,
+    0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F,
+    0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF,
+    0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61,
+    0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D
+};
 
 unsigned char * encrypt(char * input_text, char * key) {
 
@@ -160,7 +180,8 @@ void key_expansion(unsigned char * input_key, unsigned char * expanded_keys) {
 
         for (unsigned char a = 0; a < 4; a++) {
 
-            expanded_keys[bytes_generated++] = expanded_keys[bytes_generated - 16] ^ tmp[a];
+            expanded_keys[bytes_generated] = expanded_keys[bytes_generated - 16] ^ tmp[a];
+            bytes_generated++;
         }
     }
 }
@@ -208,48 +229,52 @@ void mix_columns(unsigned char * state) {
     }
 }
 
+void sub_bytes(unsigned char * state) {
 
+    for (int i = 0; i < AES_128; i++) {
 
-unsigned char * decrypt(char * input_text, char * key) {// TODO WE'RE GETTING THE KEY BACK INSTEAD OF OUT TEXT???
+        state[i] = (unsigned char) s_box[state[i]];
+    }
+}
 
-    unsigned char* output_text = (unsigned char*) malloc(sizeof(unsigned char) * (strlen(input_text) + 1));
+unsigned char * decrypt(char * input_text, char * key) {
+
+    int input_length = strlen((const char*) input_text);
+    unsigned char* output_text = (unsigned char*) malloc(sizeof(unsigned char) * (input_length + 1));
 
     unsigned char expanded_key[176];
     key_expansion((unsigned char *)key, expanded_key);
-
-    int input_length = strlen((const char*) input_text);
 
     for(int i = 0; i < input_length; i+=AES_128) {
         aes128_decrypt(input_text + i, output_text + i, expanded_key);
     }
 
+    printf("\n%s", output_text); print_hex_val_DEBUG(output_text);
+
     return output_text;
 }
 
-void aes128_decrypt(unsigned char* input_text, unsigned char* output_text, unsigned char* key) {
+void aes128_decrypt(unsigned char* input_text, unsigned char* output_text, unsigned char* key) {// TODO
 
-    unsigned char state[16];
-    for (int i = 0; i < 16; i++) {
+    unsigned char state[AES_128];
+    for (int i = 0; i < AES_128; i++) {
 		state[i] = input_text[i];
 	}
 
     add_round_key(state, key+160);
-	inverse_mix_columns(state);
 	shift_rows_right(state);
-	sub_bytes(state);
-    shift_rows_right(state);
-	sub_bytes(state);
+	sub_inverse_bytes(state);
 
-	for (int i = ROUND; i >= 0; i--) {
-        add_round_key(state, key + (16 * (i + 1)));
-	    inverse_mix_columns(state);
-	    shift_rows_right(state);
-	    sub_bytes(state);
-	}
+    for (int i = ROUND-1; i >= 0; i--) {
+        add_round_key(state, key + (AES_128 * (i + 1)));
+        inverse_mix_columns(state);
+        shift_rows_right(state);
+        sub_inverse_bytes(state);
+    }
 
-	add_round_key(state, key);
+    add_round_key(state, key);
 
-	for (int i = 0; i < 16; i++) {
+	for (int i = 0; i < AES_128; i++) {
 		output_text[i] = state[i];
 	}
 }
@@ -258,7 +283,7 @@ void inverse_mix_columns(unsigned char * state) {
 
     unsigned char tmp[4][4];
 
-    for (int i = 0; i < 16; i++) {
+    for (int i = 0; i < AES_128; i++) {
 
         tmp[i / 4][i % 4] = state[i];
     }
@@ -276,7 +301,7 @@ void inverse_mix_columns(unsigned char * state) {
       tmp[i][3] = multiply(alpha, 0x0b) ^ multiply(beta, 0x0d) ^ multiply(gamma, 0x09) ^ multiply(theta, 0x0e);
     }
 
-    for (int i = 0; i < 16; i++) {
+    for (int i = 0; i < AES_128; i++) {
 
         state[i] = tmp[i / 4][i % 4];
     }
@@ -284,16 +309,24 @@ void inverse_mix_columns(unsigned char * state) {
 
 void shift_rows_right(unsigned char * state) {
 
-    unsigned char tmp[16];
+    unsigned char tmp[AES_128];
 
-	tmp[0]  = state[0];  tmp[1]  = state[13]; tmp[2]  = state[10]; tmp[3] = state[7];
-	tmp[4]  = state[4];  tmp[5]  = state[1];  tmp[6]  = state[14]; tmp[7] = state[11];
+	tmp[0]  = state[0];  tmp[1]  = state[13]; tmp[2]  = state[10]; tmp[3] =  state[7];
+	tmp[4]  = state[4];  tmp[5]  = state[1];  tmp[6]  = state[14]; tmp[7] =  state[11];
 	tmp[8]  = state[8];  tmp[9]  = state[5];  tmp[10] = state[2];  tmp[11] = state[15];
 	tmp[12] = state[12]; tmp[13] = state[9];  tmp[14] = state[6];  tmp[15] = state[3];
 
-	for (int i = 0; i < 16; i++) {
+	for (int i = 0; i < AES_128; i++) {
 		state[i] = tmp[i];
 	}
+}
+
+void sub_inverse_bytes(unsigned char * state) {// TODO
+
+    for (int i = 0; i < AES_128; i++) {
+
+        state[i] = (unsigned char) inv_s[state[i]];
+    }
 }
 
 //ENCRYPT/DECRYPT
@@ -302,14 +335,6 @@ void add_round_key(unsigned char * state, unsigned char * round_key) {
     for (int i = 0; i < AES_128; i++) {
 
         state[i] ^= round_key[i];
-    }
-}
-
-void sub_bytes(unsigned char * state) {
-
-    for (int i = 0; i < AES_128; i++) {
-
-        state[i] = (unsigned char) s_box[state[i]];
     }
 }
 
